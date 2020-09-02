127afed29ad7bba1a04bd15fb5dbe792122a0466
maxzylam <mlam@broadinstitute.org>
2020-09-02 00:14:00 -0400

    Changes to be committed

--- SumstatsQC_util_.sh
+++ SumstatsQC_util_.sh
@@ -130,6 +130,7 @@ output=$3
             if [ -z "$FRQ" ]; then FRQ=$(zcat $sumstats | head -1 | tr ' ' '\n' | sed 's/\./_/g' | cat -n | awk '{print $2,$1}' | grep -w FRQ | awk '{print $2}'); fi
             if [ -z "$FRQ" ]; then FRQ=$(zcat $sumstats | head -1 | tr ' ' '\n' | sed 's/\./_/g' | cat -n | awk '{print $2,$1}' | grep -w MAF | awk '{print $2}'); fi
             if [ -z "$FRQ" ]; then FRQ=$(zcat $sumstats | head -1 | tr ' ' '\n' | sed 's/\./_/g' | cat -n | awk '{print $2,$1}' | grep -w AF_Allele2 | awk '{print $2}'); fi
+            if [ -z "$FRQ" ]; then FRQ=$(zcat $sumstats | head -1 | tr ' ' '\n' | sed 's/\./_/g' | cat -n | awk '{print $2,$1}' | grep -w MAF_UKB | awk '{print $2}'); fi
             if [ -z "$FRQ" ]; then echo "FRQ is not assigned...please check sumstats..."; fi 2>&1 | tee -a $output.batch.log
             if [ -z "$FRQ" ]; then FRQ=unknown; fi
             echo "FRQ $FRQ" >> $output.batch.txt
