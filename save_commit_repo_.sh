1e4e49ce3bf7efb07a26f67bed24ddf96d3f11cf
Max Lam <mlam@broadinstitute.org>
2021-08-23 12:39:27 -0400

    Changes to be committed

--- save_commit_repo_.sh
+++ save_commit_repo_.sh
@@ -1,4 +1,4 @@
 git commit -a -m "Changes to be committed"
 git push
-gsutil cp *.sh gs://summary-stats-qc-impute-1/quality-control-reffiles/WORKING_FOLDER
-gsutil cp SumstatsQC gs://summary-stats-qc-impute-1/quality-control-reffiles/WORKING_FOLDER
+#gsutil cp *.sh gs://summary-stats-qc-impute-1/quality-control-reffiles/WORKING_FOLDER
+#gsutil cp SumstatsQC gs://summary-stats-qc-impute-1/quality-control-reffiles/WORKING_FOLDER
